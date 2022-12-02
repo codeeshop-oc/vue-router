6ad75b1867d351676e368c47a1c965724ef5ca14
Anant <anantnegi8@gmail.com>
2022-12-02 16:54:09 +0530

    updated to use with framework7

--- dist/vue-router.common.js
+++ dist/vue-router.common.js
@@ -1860,9 +1860,17 @@ function handleScroll (
 function saveScrollPosition () {
   var key = getStateKey();
   if (key) {
+    var pages = document.querySelectorAll('.page-content');    
+    var y = 0;
+    if(pages && typeof pages[0] != 'undefined') {
+      y = pages[0].scrollTop;
+    }
+    
+    // x: window.pageXOffset,
+    // y: window.pageYOffset
     positionStore[key] = {
-      x: window.pageXOffset,
-      y: window.pageYOffset
+      x: 0,
+      y: y
     };
   }
 }
