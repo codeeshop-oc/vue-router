2b75b440e253940a06d76adac5e40d73a8823a8c
Anant <anantnegi8@gmail.com>
2022-12-07 16:18:51 +0530

    updated

--- dist/vue-router.esm.browser.js
+++ dist/vue-router.esm.browser.js
@@ -1837,18 +1837,17 @@ function handleScroll (
 function saveScrollPosition () {
   const key = getStateKey();
   if (key) {
-    const pages = document.querySelectorAll('.page-content');    
+    const pages = document.querySelector('.page-content');    
     let y = 0;
-    if(pages && typeof pages[0] != 'undefined') {
-      y = pages[0].scrollTop;
+    if(pages) {
+      y = pages.scrollTop;
     }
     
-    // x: window.pageXOffset,
-    // y: window.pageYOffset
     positionStore[key] = {
       x: 0,
       y: y
     };
+    console.debug(y, 'y');
   }
 }
 
@@ -1881,6 +1880,7 @@ function isValidPosition (obj) {
 }
 
 function normalizePosition (obj) {
+  console.debug(obj, 'obj normalizePosition');
   return {
     x: isNumber(obj.x) ? obj.x : window.pageXOffset,
     y: isNumber(obj.y) ? obj.y : window.pageYOffset
